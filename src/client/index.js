43e35b6df0f9fbd220820aa1a92cddf996e88d90
Suraj <55250165+SurajSanyal@users.noreply.github.com>
2022-07-30 18:48:59 -0400

    add geonames api (forreal)

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,9 @@
-import { submitWeatherQuery } from './js/app'
+import { submitWeatherQuery, submitCityQuery } from './js/app'
 
 import './styles/style.scss'
 
 alert("I'M ALIVE")
 // Event listener to add function to existing HTML DOM element
-document.getElementById('generate').addEventListener('click', submitWeatherQuery)
+document.getElementById('generate').addEventListener('click', submitCityQuery)
 
-export { submitWeatherQuery }
+export { submitWeatherQuery, submitCityQuery }
--- src/server/server.js
+++ src/server/server.js
@@ -9,8 +9,9 @@ const app = express();
 
 // Setting up API Keys from dotenv
 const dotenv = require('dotenv');
+dotenv.config();
 const GN_API_KEY = process.env.GN_API_KEY;
-const GN_BASE_URL = "http://api.geonames.org/search?q=london&maxRows=10&username=sanyal_travelapp";
+const GN_BASE_URL = "http://api.geonames.org/searchJSON?";
 
 /* Middleware */
 // Configuring express to use body-parser as middle-ware.
@@ -21,6 +22,11 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
+
+// node-fetch to avoid "fetch is not defined" error
+const fetch = require('node-fetch');
+// luxon for date processing
+const { DateTime } = require("luxon");
 
 // Initialize the main project folder (built into 'dist' folder)
 app.use(express.static('dist'));
@@ -54,3 +60,22 @@ app.post('/addData', (req, res) => {
     }
 })
 
+/* Function to query Geonames w/ POST */
+app.post("/geonamesData", async (req, res) => {
+    console.log(req.body);
+    const city = req.body.city;
+    console.log(
+        "url = ",
+        GN_BASE_URL + "q=" + city + "&maxRows=1&username=" + GN_API_KEY
+    );
+    const url = GN_BASE_URL + "q=" + city + "&maxRows=1&username=" + GN_API_KEY;
+
+    const cityData = await fetch(url);
+
+    try {
+        const response = await cityData.json();
+        res.send(response);
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+})
